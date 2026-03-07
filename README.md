343334a4def9d5be6f36e49a6ea4e1f8b53f7b10
Brian <brianmp777@gmail.com>
2026-03-06 22:45:28 -0300

    final de proyecto y subo las imagenes para documentar

--- README.md
+++ README.md
@@ -63,4 +63,114 @@ api-colegio/
 - Documentación FastAPI
 - Documentación SQLModel
 - Pydantic Validators
+
+📷 Documentación visual de endpoints
+Las siguientes capturas corresponden a la documentación automática generada por FastAPI Swagger UI.
+
+Documentación online:
+https://api-colegio-baig.onrender.com/docs
+
+Documentación local:
+http://127.0.0.1:8000/docs
+
+
+
+- Endpoints relacionados con la gestión de alumnos.
+
+![GET get_alumnos](imgs_readme/alumnos/get_alumnos.png)
+
+![GET get_alumno](imgs_readme/alumnos/get_alumno.png)
+
+![GET get_alumno_en_curso](imgs_readme/alumnos/get_alumno_en_curso.png)
+
+![POST asignar_curso_alumno](imgs_readme/alumnos/asignar_curso_alumno.png)
+
+![PUT update_alumno](imgs_readme/alumnos/update_alumno.png)
+
+![DELETE delete_alumno](imgs_readme/alumnos/delete_alumno.png)
+
+![DELETE delete_alumno_de_curso](imgs_readme/alumnos/delete_alumno_de_curso.png)
+
+- Endpoints relacionados con la gestión Autentificacion, Creacion de Cuenta, e Inicio de Sesion para los roles.
+
+![GET obtener_datos_usuario_logueado](imgs_readme/auth/obtener_datos_usuario_logueado.png)
+
+![POST recuperar_contrasenia](imgs_readme/auth/recuperar_contrasenia.png)
+
+![POST register_alumno](imgs_readme/auth/register_alumno.png)
+
+![POST register_profesor](imgs_readme/auth/register_profesor.png)
+
+![PUT subir_foto_perfil_o_modificar](imgs_readme/auth/subir_foto_perfil_o_modificar.png)
+
+![POST verificar_credenciales](imgs_readme/auth/verificar_credenciales.png)
+
+- Endpoints relacionados con la gestión de cursos en el sistema
+
+![GET get_cursos](imgs_readme/cursos/get_cursos.png)
+
+![GET get_curso](imgs_readme/cursos/get_curso.png)
+
+![POST create_curso](imgs_readme/cursos/create_curso.png)
+
+![PUT update_curso](imgs_readme/cursos/update_curso.png)
+
+![DELETE delete_curso](imgs_readme/cursos/delete_curso.png)
+
+- Endpoints relacionados con la gestión de materias en el sistema
+
+![GET get_materias](imgs_readme/materias/get_materias.png)
+
+![GET get_materia](imgs_readme/materias/get_materia.png)
+
+![POST create_materia](imgs_readme/materias/create_materia.png)
+
+![PUT update_materia](imgs_readme/materias/update_materia.png)
+
+![DELETE delete_materia](imgs_readme/materias/delete_materia.png)
+
+- Endpoints relacionados con la gestión de la relacion materia_curso en el sistema
+
+![GET get_materias_curso](imgs_readme/materiasCurso/get_materias_curso.png)
+
+![POST create_materia_curso](imgs_readme/materiasCurso/create_materia_curso.png)
+
+![DELETE delete_materia_curso](imgs_readme/materiasCurso/delete_materia_curso.png)
+
+- Endpoints relacionados con la gestión de profesores.
+
+![GET get_profesores](imgs_readme/profesores/get_profesores.png)
+
+![GET get_profesor](imgs_readme/profesores/get_profesor.png)
+
+![GET get_profesor_en_curso_materia](imgs_readme/profesores/get_profesor_en_curso_materia.png)
+
+![POST asignar_curso_y_materia_profesor](imgs_readme/profesores/asignar_curso_y_materia_profesor.png)
+
+![PUT update_profesor](imgs_readme/profesores/update_profesor.png)
+
+![DELETE delete_profesor](imgs_readme/profesores/delete_profesor.png)
+
+![DELETE delete_profesor_de_curso_materia](imgs_readme/profesores/delete_profesor_de_curso_materia.png)
+
+- Endpoints relacionados con la gestión de publicaciones, entregas, subidas de archivos.
+
+![GET leer_datos_publicacion](imgs_readme/publicaciones/leer_datos_publicacion.png)
+
+![GET leer_archivos_tarea_o_material](imgs_readme/publicaciones/leer_archivos_tarea_o_material.png)
+
+![GET descargar_archivo](imgs_readme/publicaciones/descargar_archivo.png)
+
+![POST crear_publicacion](imgs_readme/publicaciones/crear_publicacion.png)
+
+![POST entregar_tarea_y_subir_su_archivo](imgs_readme/publicaciones/entregar_tarea_y_subir_su_archivo.png)
+
+![POST subir_archivos_material_o_tarea](imgs_readme/publicaciones/subir_archivos_material_o_tarea.png)
+
+![PUT actualizar_publicacion](imgs_readme/publicaciones/actualizar_publicacion.png)
+
+![PUT coreccion_tarea](imgs_readme/publicaciones/coreccion_tarea.png)
+
+![DELETE eliminar_publicacion](imgs_readme/publicaciones/eliminar_publicacion.png)
+
 ```
--- routers/materias.py
+++ routers/materias.py
@@ -10,6 +10,17 @@ router = APIRouter(prefix="/materias", tags=["Materias"])
 
 @router.get("/", response_model=list[MateriaOut])
 def get_materias(session: Session = Depends(get_session), user=Depends(require_role(PerfilUsuario.admin))) -> list[MateriaOut]:
+    """
+    Endpoint para obtener todos las materias registrados en el sistema.
+
+    Requiere que el usuario autenticado tenga rol de administrador.
+
+    - **session**: Sesión de base de datos obtenida mediante la dependencia get_session.
+    - **user**: Usuario autenticado validado mediante require_role, que asegura que tenga rol de administrador.
+
+    Retorna una lista de objetos **MateriaOut** con la información de cada materia registrada,
+    incluyendo nombre, codigo, activo , descripcion , fechas de creación y modificación.
+    """
     materias = session.query(Materia).all()
     respuesta = []
     for materia in materias:
@@ -19,7 +30,22 @@ def get_materias(session: Session = Depends(get_session), user=Depends(require_r
     return respuesta
 
 @router.get("/{materia_id}", response_model=MateriaOut)
-def get_materia(materia_id: UUID, session: Session = Depends(get_session), user=Depends(require_role(PerfilUsuario.admin))) -> MateriaOut:
+def get_materia(materia_id: UUID, session: Session = Depends(get_session), 
+                user=Depends(require_role(PerfilUsuario.admin))) -> MateriaOut:
+    """
+    Endpoint para obtener una materia específica a partir de su ID.
+
+    Requiere autenticación y rol de administrador.
+
+    - **materia_id**: Identificador único del materia que se desea consultar.
+    - **session**: Sesión de base de datos proporcionada por la dependencia get_session.
+    - **user**: Usuario autenticado validado mediante require_role.
+
+    Retorna un objeto **MateriaOut** con los datos de la materia solicitado.
+
+    Si la materia no existe, lanza una excepción HTTP 404.
+    Si ocurre un error inesperado en el servidor, lanza una excepción HTTP 500.
+    """
     try:
         materia = session.get(Materia, materia_id)
         if not materia:
@@ -33,7 +59,22 @@ def get_materia(materia_id: UUID, session: Session = Depends(get_session), user=
         raise HTTPException(status_code=500, detail=f"Error al obtener materia: {str(e)}")
 
 @router.post("/", response_model=MateriaOut)
-def create_materia(materia_data: MateriaCreate, session: Session = Depends(get_session), user=Depends(require_role(PerfilUsuario.admin))) -> MateriaOut:
+def create_materia(materia_data: MateriaCreate, session: Session = Depends(get_session), 
+                    user=Depends(require_role(PerfilUsuario.admin))) -> MateriaOut:
+    """
+    Endpoint para obtener una materia específica a partir de su ID.
+
+    Requiere autenticación y rol de administrador.
+
+    - **materia_id**: Identificador único del materia que se desea consultar.
+    - **session**: Sesión de base de datos proporcionada por la dependencia get_session.
+    - **user**: Usuario autenticado validado mediante require_role.
+
+    Retorna un objeto **MateriaOut** con los datos del materia solicitado.
+
+    Si el materia no existe, lanza una excepción HTTP 404.
+    Si ocurre un error inesperado en el servidor, lanza una excepción HTTP 500.
+    """
     try:
         nueva_materia = Materia(nombre=materia_data.nombre, codigo=materia_data.codigo, descripcion=materia_data.descripcion)
         session.add(nueva_materia)
@@ -50,6 +91,22 @@ def create_materia(materia_data: MateriaCreate, session: Session = Depends(get_s
 @router.put("/{materia_id}", response_model=MateriaOut)
 def update_materia(materia_id: UUID, materia_data: MateriaUpdate, session: Session = Depends(get_session), 
                     user=Depends(require_role(PerfilUsuario.admin))) -> MateriaOut:
+    """
+    Endpoint para actualizar una materia existente.
+
+    Requiere autenticación y rol de administrador.
+
+    - **materia_id**: ID de la materia que se desea actualizar.
+    - **materia_data**: Objeto del esquema MateriaUpdate que contiene los nuevos valores
+      para los campos de la materia (nombre, descripcion, codigo y activo).
+    - **session**: Sesión de base de datos obtenida mediante la dependencia get_session.
+    - **user**: Usuario autenticado validado mediante require_role.
+
+    Retorna un objeto **MateriaOut** con los datos actualizados del materia.
+
+    Si la materia no existe, lanza una excepción HTTP 404.
+    Si ocurre un error inesperado durante la actualización, lanza una excepción HTTP 500.
+    """
     try:
         materia = session.get(Materia, materia_id)
         if not materia:
@@ -71,7 +128,22 @@ def update_materia(materia_id: UUID, materia_data: MateriaUpdate, session: Sessi
         raise HTTPException(status_code=500, detail=f"Error al actualizar materia: {str(e)}")
 
 @router.delete("/{materia_id}")
-def delete_materia(materia_id: UUID, session: Session = Depends(get_session), user=Depends(require_role(PerfilUsuario.admin))) -> dict:
+def delete_materia(materia_id: UUID, session: Session = Depends(get_session), 
+                    user=Depends(require_role(PerfilUsuario.admin))) -> dict:
+    """
+    Endpoint para eliminar una materia del sistema.
+
+    Requiere autenticación y rol de administrador.
+
+    - **materia_id**: Identificador único de la materia que se desea eliminar.
+    - **session**: Sesión de base de datos obtenida mediante la dependencia get_session.
+    - **user**: Usuario autenticado validado mediante require_role.
+
+    Si la materia existe, se elimina de la base de datos y se devuelve un mensaje de confirmación.
+
+    Si la materia no existe, lanza una excepción HTTP 404.
+    Si ocurre un error inesperado durante la eliminación, lanza una excepción HTTP 500.
+    """
     try:
         materia = session.get(Materia, materia_id)
         if not materia:
